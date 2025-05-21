bbf78379fd3926718a7ae6a230e68b5ba5a121a1
Szymon Cybulski <szymcyb04@protonmail.com>
2025-05-21 15:08:52 +0100

    Add token.py: define token types and token class

---
+++ source/token.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from typing import Any
+
+class TokenType(Enum):
+    # literal types
+    NUMBER = 'NUMBER'
+    INTEGER = 'INT'
+    FLOAT = 'FLOAT'
+    
+    # single-character tokens operators
+    PLUS = '+'
+    MINUS = '-'
+    STAR = '*'
+    SLASH = '/'
+
+    #single-character tokens grouping
+    LEFT_PAREN = '('
+    RIGHT_PAREN = ')'
+
+    EOF = ''
+
+class Token:
+    def __init__(
+            self,
+            typ: TokenType,
+            lexeme: str,
+            literal: Any = None,
+            line: int = 0
+    ) -> None:
+        self.type = typ
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
